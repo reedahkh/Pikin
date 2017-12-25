c43bb677ed218c602c38cdb54cac845837cbc0c2
Vincent Nwonah <vnwonah@outlook.com>
2017-12-25 13:49:00 +0100

    updated booking

--- verify.php
+++ verify.php
@@ -16,46 +16,50 @@
 
 <?php
          
-session_start();
-include("dbconnection.php");
-            
-if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash']));
-    // Verify data
-    $email = ($_GET['email']); 
-    $hash = ($_GET['hash']); 
+    session_start();
+    include("dbconnection.php");
+                
+    if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash']));
+        // Verify data
+        $email = ($_GET['email']); 
+        $hash = ($_GET['hash']); 
+        echo $email;
+        echo $hash;
 
-    $sql = mysqli_query("SELECT email, hash, active FROM parents WHERE email='$email', hash='$hash' AND active='0'"); 
-    $result = mysqli_query ($connection, $sql);
+        $sql = "SELECT email, hash, active, FROM parents WHERE email='$email' AND hash='$hash' AND active='0'"; 
+        $result = mysqli_query ($connection, $sql);
 
-    if (mysqli_num_rows($result) > 0) {
-        $sqlx = "UPDATE parents SET active='1' WHERE email='$email', hash='$hash', active='0'";
-        $resultx = mysqli_query ($connection, $sqlx);
-    
-        if (mysqli_num_rows($resultx)>0) {
+        if (mysqli_num_rows($result) > 0) {
+            $sqlx = "UPDATE parents SET active='1' WHERE email='$email', hash='$hash', active='0'";
+            $resultx = mysqli_query ($connection, $sqlx);
+        
+            if (mysqli_num_rows($resultx)>0) {
 
-            while ($row = mysqli_fetch_assoc ($resultx)){
+                while ($row = mysqli_fetch_assoc ($resultx)){
 
-            
-            echo 'Your account has been activated, you can now login ';
-            header('location:setup.php');
+                
+                echo 'Your account has been activated, you can now login ';
+                header('location:setup.php');
 
+                }
             }
-        }
-        else {
-       
-        echo 'The url is either invalid or you already activated your account';
-        }
-                 
-} 
 
-else {
-    echo 'Invalid approach, please use the link that has been sent to your email.';
-}   
-            mysqli_free_result ($result);
-                mysqli_close ($connection);
-                else 
-                  die(header("location:signup.php"));
-        ?>
+            else {
+                echo 'The url is either invalid or you already activated your account';
+            }
+                    
+    } 
+
+    elseif (true) {
+        echo 'Invalid approach, please use the link that has been sent to your email.';
+        mysqli_free_result ($result);
+        mysqli_close ($connection);
+
+    }
+    else {
+        die(header("location:signup.php"));
+    }
+?>
        
  
          
