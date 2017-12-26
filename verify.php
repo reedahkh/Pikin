6fed07d576e33522b75260fa62018cedff3b5846
Vincent Nwonah <vnwonah@outlook.com>
2017-12-26 19:00:56 +0100

    modified verification page

--- verify.php
+++ verify.php
@@ -18,31 +18,31 @@
     include("dbconnection.php");
    
                 
-    if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash']));
+    if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])) {
         // Verify data
         $email = ($_GET['email']); 
         $hash = ($_GET['hash']); 
-        
         $sql = "SELECT * FROM parents WHERE email='$email' AND hashkey='$hash' AND active='0'"; 
         echo $sql;
         $result = mysqli_query($connection, $sql);
-        print_r($result);
         $rowcount=mysqli_num_rows($result);
-        echo $rowcount;
         if($rowcount > 0) {
             $sqlx = "UPDATE parents SET active='1' WHERE email='$email' AND hashkey='$hash' AND active='0'";
             if ($connection->query($sqlx) === TRUE) {
                 echo 'Your account has been activated, you can now login ';
                 $_SESSION['email'] = $email;
                 header('location:setup.php');
-            } else {
+            
+            } 
+            
+            else {
                 echo "Error: " . $sql . "<br>" . $connection->error;
             }
         }
-            
-        
-          
-            
+             
+         
+    }
+ 
     elseif ($rowcount < 0) {
        
         echo 'The url is either invalid or you already activated your account';
