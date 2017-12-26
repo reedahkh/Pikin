b20b9e4b837273a89e76444e726fe63f2863f848
Vincent Nwonah <vnwonah@outlook.com>
2017-12-26 17:28:23 +0100

    modified register.php

--- register.php
+++ register.php
@@ -1,4 +1,27 @@
 <?php
+	include("dbconnection.php"); //creates database connection
 
-echo "Hello world!";
+	if(isset($_POST["firstname"])) {
+		$firstname = $_POST ['firstname']; 
+		$lastname = $_POST ['lastname']; 
+		$email = $_POST ['email']; 
+		$password = $_POST ['password']; 
+		$paymentmethod = $_POST ['paymentmethod'];
+		$hash = md5(rand (0,1000));
+
+		$sql = "INSERT INTO parents (firstname, lastname, email, password, paymentmethod, hashkey) VALUES ('$firstname', '$lastname', '$email', 'SHA1 ($password)', '$paymentmethod', '$hash')";
+		if ($connection->query($sql) === TRUE) {
+			echo "New record created successfully";
+		} else {
+			echo "Error: " . $sql . "<br>" . $connection->error;
+		}
+	}
+
+	else {
+		die(header('Location: index.php'));
+	}
+
+
+
+
 ?>
