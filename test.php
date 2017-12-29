e0eba02ce54ced16c51d76940b3a7fcc1288020c
reedahkh <fareedakabeer@gmail.com>
2017-12-29 15:05:16 +0100

    new login script

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include("dbconnection.php"); //creates database connection
+if ($_POST['submit']!= "") { 
+$email = $_POST ['email'];
+$password = $_POST ['password'];
+//Executes the query
+$sql = "SELECT * from parents where email = '$email' and password = 'SHA1 ($password)' ";
+$result = mysqli_query ($connection, $sql);
+if (mysqli_num_rows($result)>0){
+		while ($row = mysqli_fetch_assoc ($result)) {
+		$_SESSION['ParentID'] = $row['ParentID'];
+		header("Location:pmember.php");
+	}
+}	
+	
+}
+else 
+	header('Location:signin.php?loginFailed=true&reason=password'); 
+?>
