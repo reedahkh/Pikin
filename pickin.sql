c65e8ef7fdf8fc54ae2d3188724f413a52425964
reedahkh <fareedakabeer@gmail.com>
2017-12-31 12:16:59 +0100

    new sql

--- pickin.sql
+++ pickin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 30, 2017 at 07:31 PM
+-- Generation Time: Dec 31, 2017 at 12:16 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.6
 
@@ -60,8 +60,8 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`BookingID`, `ParentID`, `EducatorID`, `postcode`, `suburb`, `typeofservice`, `day`, `starttime`, `endtime`, `paymentmethod`, `timestamp`) VALUES
-(1, 10, 0, 5006, 'North Adelaide, SA', 'Morning Childcare Service', 'monday ,', '08:00:00.000000', '09:00:00.000000', 'Paypal', '2017-12-30 17:26:46.445208'),
-(2, 10, 0, 5006, 'North Adelaide, SA', 'Morning Childcare Service', 'monday , tuesday ,', '08:00:00.000000', '09:00:00.000000', 'Paypal', '2017-12-30 17:26:46.445208'),
+(1, 12, 11, 5006, 'North Adelaide, SA', 'Morning Childcare Service', 'monday ,', '08:00:00.000000', '09:00:00.000000', 'Paypal', '2017-12-30 17:26:46.445208'),
+(2, 12, 11, 5006, 'North Adelaide, SA', 'Morning Childcare Service', 'monday , tuesday ,', '08:00:00.000000', '09:00:00.000000', 'Paypal', '2017-12-30 17:26:46.445208'),
 (3, 10, 0, 5006, 'gavin', 'Morning Childcare Service', 'monday ,', '08:00:00.000000', '09:00:00.000000', 'Credit Card', '2017-12-30 17:26:46.445208'),
 (4, 10, 0, 444, 'ffff', 'Morning Childcare Service', 'monday ,', '08:00:00.000000', '09:00:00.000000', 'Credit Card', '2017-12-30 17:26:46.445208');
 
@@ -81,7 +81,6 @@ CREATE TABLE `coordinators` (
   `suburb` varchar(50) NOT NULL,
   `postcode` int(20) NOT NULL,
   `avatar` varchar(250) NOT NULL DEFAULT 'default.jpg',
-  `number` varchar(50) NOT NULL,
   `password` varchar(30) NOT NULL,
   `hashkey` varchar(32) NOT NULL,
   `active` int(1) NOT NULL DEFAULT '0'
@@ -91,8 +90,9 @@ CREATE TABLE `coordinators` (
 -- Dumping data for table `coordinators`
 --
 
-INSERT INTO `coordinators` (`CoordinatorID`, `firstname`, `lastname`, `phonenumber`, `email`, `address`, `suburb`, `postcode`, `avatar`, `number`, `password`, `hashkey`, `active`) VALUES
-(2, 'James', 'Hope', '', 'james@yahoo.com', '29 Earl Street', 'North Adelaide, SA', 5006, 'default.jpg', '+2348038471835', 'james', '', 1);
+INSERT INTO `coordinators` (`CoordinatorID`, `firstname`, `lastname`, `phonenumber`, `email`, `address`, `suburb`, `postcode`, `avatar`, `password`, `hashkey`, `active`) VALUES
+(2, 'James', 'Hope', '+2348038471835', 'james@yahoo.com', '29 Earl Street', 'North Adelaide, SA', 5006, 'default.jpg', 'james', '', 1),
+(6, 'Farida', 'Kabir', '+2348038471835', 'fareedakabeer@gmail.com', 'James Street', 'Gavin', 5113, 'default.jpg', 'SHA1 (f)', '', 1);
 
 -- --------------------------------------------------------
 
@@ -135,7 +135,8 @@ INSERT INTO `educators` (`EducatorID`, `firstname`, `lastname`, `email`, `phonen
 (9, 'Nora', 'Roberts', 'nora@yahoo.com', '+234596873086', 'nora', '', 3814, 'Garfield, Vic', 2, 3, 'default.jpg', 1, '', '', '', '', ''),
 (10, 'Farida', 'Kabir', 'fareedakabeer@icloud.com', '', 'SHA1 (ff)', '7e7757b1e12abcb736ab9a754ffb617a', 0, '', 0, 0, 'default.jpg', 1, '', '', '', '', ''),
 (11, 'Amira', 'Kabir', 'amira@gmail.com', '', 'SHA1 (a)', '202cb962ac59075b964b07152d234b70', 5006, 'Gavin', 0, 0, 'default.jpg', 1, '32, Thames Street', '', '', '', ''),
-(12, 'tt', 'tt', 't@yahoo.com', '', 'SHA1 (t)', '1ecfb463472ec9115b10c292ef8bc986', 0, '', 0, 0, 'default.jpg', 1, '', '', '', '', '');
+(12, 'tt', 'tt', 't@yahoo.com', '', 'SHA1 (t)', '1ecfb463472ec9115b10c292ef8bc986', 0, '', 0, 0, 'default.jpg', 1, '', '', '', '', ''),
+(13, 'Farida', 'Kabir', 'fareedakabeer@gmail.com', '', 'SHA1 (f)', '', 0, '', 0, 0, 'default.jpg', 0, '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -161,7 +162,7 @@ CREATE TABLE `parents` (
   `officepostcode` varchar(50) NOT NULL,
   `image` varchar(200) NOT NULL,
   `numkids` int(10) NOT NULL,
-  `ageofkids` int(30) NOT NULL,
+  `ageofkids` varchar(30) NOT NULL,
   `typeofservice` text NOT NULL,
   `servicehours` varchar(50) NOT NULL,
   `active` int(1) NOT NULL DEFAULT '0'
@@ -172,8 +173,8 @@ CREATE TABLE `parents` (
 --
 
 INSERT INTO `parents` (`ParentID`, `firstname`, `lastname`, `email`, `age`, `password`, `hashkey`, `paymentmethod`, `phonenumber`, `homeaddress`, `homesuburb`, `homepostcode`, `officeaddress`, `officesuburb`, `officepostcode`, `image`, `numkids`, `ageofkids`, `typeofservice`, `servicehours`, `active`) VALUES
-(12, 'Amira', 'Kabir', 'amira@yahoo.com', 24, 'SHA1 (a)', '', 'Paypal', '+2348038471835', 'No 20 Yusuf Abdulsalam Street', 'North Adelaide, SA', '5006', '252 Winston Street', 'Gavin', '4211', 'default.jpg', 1, 4, 'Drop off and Pick up', '2', 1),
-(30, 'Farida', 'Kabir', 'fareedakabeer@gmail.com', 0, 'SHA1 (f)', '49ae49a23f67c759bf4fc791ba842aa2', 'Credit Card', '', 'John Street', 'Adelaide', '5006', '', '', '', 'default.jpg', 0, 0, '', '', 1);
+(12, 'Amira', 'Kabir', 'amira@yahoo.com', 24, 'SHA1 (a)', '', 'Paypal', '+2348038471835', 'No 20 Yusuf Abdulsalam Street', 'North Adelaide, SA', '5006', '252 Winston Street', 'Gavin', '4211', 'default.jpg', 1, '4', 'Drop off and Pick up', '2', 1),
+(30, 'Farida', 'Kabir', 'fareedakabeer@gmail.com', 0, 'SHA1 (f)', '49ae49a23f67c759bf4fc791ba842aa2', 'Credit Card', '', 'John Street', 'Adelaide', '5006', '', '', '', 'default.jpg', 0, '0', '', '', 1);
 
 --
 -- Indexes for dumped tables
@@ -189,8 +190,7 @@ ALTER TABLE `admin`
 -- Indexes for table `booking`
 --
 ALTER TABLE `booking`
-  ADD PRIMARY KEY (`BookingID`),
-  ADD KEY `ParentID` (`ParentID`);
+  ADD PRIMARY KEY (`BookingID`);
 
 --
 -- Indexes for table `coordinators`
@@ -230,12 +230,12 @@ ALTER TABLE `booking`
 -- AUTO_INCREMENT for table `coordinators`
 --
 ALTER TABLE `coordinators`
-  MODIFY `CoordinatorID` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `CoordinatorID` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `educators`
 --
 ALTER TABLE `educators`
-  MODIFY `EducatorID` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `EducatorID` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `parents`
 --
